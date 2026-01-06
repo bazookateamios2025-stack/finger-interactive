5e11fac4c7cb8ce48c03d11ab986726c327ec3a0
mailoan <bazooka.loanmtk@gmail.com>
2026-01-06 15:58:07 +0700

    update

--- trending/a.py
+++ trending/a.py
@@ -1,13 +1,13 @@
 import requests
 
-START = 1
-END = 44
+START = 11
+END = 16
 
-BASE_URL = "https://storage.googleapis.com/finger-interactive/finger_interactive/thumb/{}.webp"
+BASE_URL = "https://storage.googleapis.com/finger-interactive/finger_interactive/{}.mp4"
 
 for i in range(START, END + 1):
     url = BASE_URL.format(i)
-    filename = f"{i}.webp"
+    filename = f"{i}.mp4"
 
     print(f"Downloading {url} -> {filename}")
 
